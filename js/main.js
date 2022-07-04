63dc82ba71a1493dda95894a8f1811a5e8183dbe
nezaga <nicolas.zaga@gmail.com>
2022-07-04 18:37:29 -0300

    Agreado de objetos, arrays, funciones, recorrido de arrays

--- js/main.js
+++ js/main.js
@@ -12,16 +12,78 @@ let sumaAccidente = 2000000;
 let sumaVida = 10000000;
 let sumaSalud = 1000000;
 
+
+//Definimos la clase Cobertura.
+class Cobertura {
+    constructor(nombre, SA) {
+        this.nombre = nombre;
+        this.sumaAsegurada = SA;
+        this.prima = 0.0;
+    }
+    calcularPrima(tasa) {
+        this.prima = this.sumaAsegurada * tasa;
+    }
+}
+
+// Creo los 3 objetos Coberturas y se agrega al array
+function elegirCobAccidente(seleccionCob) {
+    quiereCobAccidente = prompt("¿Desea tener cobertura por accidente? Ingrese S o N").toUpperCase();
+    cob_accidente = stringABool(quiereCobAccidente, cob_accidente);
+    if (cob_accidente == true) {
+        const cobertura = new Cobertura("Accidente", sumaAccidente);
+        cobertura.calcularPrima(tasa);
+        seleccionCob.push(cobertura);
+        alert("Cobertura seleccionada correctamente");
+    } else {
+        alert("Cobertura no seleccionada");
+    }
+}
+
+function elegirCobVida(seleccionCob) {
+    quiereCobVida = prompt("¿Desea tener cobertura por seguro de vida? Ingrese S o N").toUpperCase();
+    cob_vida = stringABool(quiereCobVida, cob_vida);
+    if (cob_vida == true) {
+        const cobertura = new Cobertura("Vida", sumaVida);
+        cobertura.calcularPrima(tasa);
+        seleccionCob.push(cobertura);
+        alert("Cobertura seleccionada correctamente");
+    } else {
+        alert("Cobertura no seleccionada");
+    }
+}
+
+function elegirCobSalud(seleccionCob) {
+    quiereCobSalud = prompt("¿Desea tener cobertura de medicina prepaga? Ingrese S o N").toUpperCase();
+    cob_salud = stringABool(quiereCobSalud, cob_salud);
+    if (cob_salud == true) {
+        const cobertura = new Cobertura("Salud", sumaSalud);
+        cobertura.calcularPrima(tasa);
+        seleccionCob.push(cobertura);
+        alert("Cobertura seleccionada correctamente");
+    } else {
+        alert("Cobertura no seleccionada");
+    }
+}
+
+//Definimos un array vacío, para guardar la selección del usuario
+const seleccionCob = [];
+
 //Bienvenida y preguntas iniciales
-function obtenerDatos () {
-edad = prompt("Comencemos ¿Qué edad tenés?");
-quiereCobAccidente =  prompt("¿Desea tener cobertura por accidente? Ingrese S o N").toUpperCase();
-quiereCobVida = prompt("¿Desea tener cobertura por seguro de vida? Ingrese S o N").toUpperCase();
-quiereCobSalud = prompt("¿Desea tener cobertura de medicina prepaga? Ingrese S o N").toUpperCase();
+function obtenerDatos() {
+    edad = prompt("Comencemos ¿Qué edad tenés?");
+    if (validarEdadYObtenerTasa(edad)) {
+        elegirCobAccidente(seleccionCob);
+        elegirCobVida(seleccionCob);
+        elegirCobSalud(seleccionCob);
+        return true;
+    } else {
+        alert("Gracias por visitarnos.");
+        return false;
+    }
 }
 
 alert("Bienvenido al simulador para cotizar tu seguro, pulsa 'aceptar' para iniciar tu cotización");
-obtenerDatos();
+//obtenerDatos();
 
 //Traducimos los datos ingresados a inputs para cotizar
 function stringABool(par1, par2) {
@@ -34,19 +96,21 @@ function stringABool(par1, par2) {
     return par2;
 }
 
-cob_accidente = stringABool(quiereCobAccidente, cob_accidente);
-cob_vida = stringABool(quiereCobVida, cob_vida);
-cob_salud = stringABool(quiereCobSalud, cob_salud);
-
 // Validamos edad y determinamos la tasa
-if (edad < 18) {
-    alert("No tenés edad para contratar un seguro, lo sentimos.");
-} else if ((edad >= 18 ) && (edad < 45)) {
-    tasa = 0.02;
-} else if ((edad >= 45 ) && (edad <= 65)) {
-    tasa = 0.035;
-} else {
-    alert("Superaste la edad de asegurabilidad, lo sentimos.");
+function validarEdadYObtenerTasa(edad) {
+    if (edad < 18) {
+        alert("No tenés edad para contratar un seguro, lo sentimos.");
+        return false;
+    } else if ((edad >= 18) && (edad < 45)) {
+        tasa = 0.02;
+        return true;
+    } else if ((edad >= 45) && (edad <= 65)) {
+        tasa = 0.035;
+        return true;
+    } else {
+        alert("Superaste la edad de asegurabilidad, lo sentimos.");
+        return false;
+    }
 }
 
 console.log(edad);
@@ -54,46 +118,47 @@ console.log(cob_accidente);
 console.log(cob_vida);
 console.log(cob_salud);
 console.log(tasa);
+console.log(seleccionCob);
+
 
 // Proceso de cotización
-function calcularPremio(cobertura, suma, tasa) {
-    if (cobertura == true) {
-        let prima = suma * tasa;
-        return total = prima + prima * iva; 
+// Recorremos el array de coberturas seleccionadas, sumando la prima de cada una y calculando el Premio Total agreando el IVA
+function calcularPremio() {
+    const primaTotal = seleccionCob.reduce((acc, el) => acc + el.prima, 0)
+    return premioTotal = primaTotal + primaTotal * iva;
+}
+
+// Función de Pago
+function pago() {
+    // Se solicita ingreso de cuotas
+    let cuotas = parseInt(prompt("El total a pagar es $" + premioTotal + " ¿En cuántas cuotas desea pagar?"));
+
+    // Se calcula el costo financiero, según la cantidad de cuotas elegidas
+    let premioTotalFinanciado = 0;
+
+    if (cuotas != 1) {
+        for (let i = 1; i <= cuotas; i++) {
+            let precioPorCuota = premioTotal / cuotas;
+            alert("La cuota " + i + " será de $" + precioPorCuota);
+            premioTotal = premioTotal * 1.03;
+            premioTotalFinanciado += precioPorCuota;
+            console.log(i);
+            console.log(precioPorCuota);
+            console.log(premioTotalFinanciado);
+        }
+        alert("El precio final es de $" + premioTotalFinanciado + " a pagar en " + cuotas + " cuotas ¡Muchas gracias!");
+
     } else {
-        return total = 0;
+        alert("El precio final es de $" + premioTotal + " a pagar en 1 cuota ¡Muchas gracias!");
     }
 }
 
-let totAccidente = calcularPremio(cob_accidente, sumaAccidente, tasa);
-let totVida = calcularPremio(cob_vida, sumaVida, tasa);
-let totSalud = calcularPremio(cob_salud, sumaSalud, tasa);
-let premioTotal = totAccidente + totVida + totSalud;
 
-console.log(totAccidente);
-console.log(totVida);
-console.log(totSalud);
-console.log(premioTotal);
-
-// Se solicita ingreso de cuotas
-let cuotas = parseInt(prompt("El total a pagar es $" + premioTotal + " ¿En cuántas cuotas desea pagar?"));
-
-// Se calcula el costo financiero, según la cantidad de cuotas elegidas
-let premioTotalFinanciado = 0;
-
-if (cuotas != 1) {
-    for (let i = 1; i <= cuotas; i++) {
-        let precioPorCuota = premioTotal / cuotas;
-        alert("La cuota " + i + " será de $" + precioPorCuota);
-        premioTotal = premioTotal * 1.03;
-        premioTotalFinanciado += precioPorCuota;
-        console.log(i);
-        console.log(precioPorCuota);
-        console.log(premioTotalFinanciado);
-    }
-    alert("El precio final es de $" + premioTotalFinanciado + " a pagar en " + cuotas + " cuotas ¡Muchas gracias!");
-    
+// Se llama a las funciones de cotización y pago, sólo en caso de que los datos obtenidos hayan sido válidos
+if (obtenerDatos() == true) {
+    premioTotal = calcularPremio();
+    console.log(premioTotal);
+    pago();
 } else {
-    alert("El precio final es de $" + premioTotal + " a pagar en 1 cuota ¡Muchas gracias!");
+    console.log("No pudo cotizar");
 }
-
